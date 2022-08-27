97c7b996f02485b7aea3933b9dd731e06a79324a
Murad <muradaliyev2229@gmail.com>
2022-08-27 23:23:10 +0400

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const sequelize = new Sequelize(
-  `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+  `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
   {
    dialectOptions: {
       ssl: {
